18024252cd1825c3ba14c3021ef9967c5676a401
Yongzheng Huang <iamhuangyz@gmail.com>
2015-05-31 16:00:16 -0400

    buggy

--- Burrows-Wheeler/src/MoveToFront.java
+++ Burrows-Wheeler/src/MoveToFront.java
@@ -1,19 +1,31 @@
 import java.util.HashMap;
 
-public class MoveToFront {		
+public class MoveToFront {
 	// apply move-to-front encoding, reading from standard input and writing to
 	// standard output
 	public static void encode() {
-		HashMap<Character, Integer> sequence = new HashMap<Character,Integer>(256);
+		HashMap<Character, Integer> sequence = new HashMap<Character, Integer>(
+				256);
+		char first = (char) 0;
 		for (int i = 0; i < 256; i++) {
-			sequence.put((char) i, i); 
-			System.out.printf("%c -- %d\n", (char) i, sequence.get((char) i));
-		}
+			sequence.put((char) i, i);
+		}		
 		while (!BinaryStdIn.isEmpty()) {
 			char c = BinaryStdIn.readChar(8);
 			int index = sequence.get(c);
-			BinaryStdOut.write(index);
+			// BinaryStdOut.write(index);			
+			System.out.println(index);
+			exch(first, c, sequence);
+			first = c;
 		}
+		// BinaryStdOut.flush();
+	}
+
+	private static void exch(char first, char c,
+			HashMap<Character, Integer> sequence) {
+		int temp = sequence.get(first);
+		sequence.put(first, sequence.get(c));
+		sequence.put(c, temp);
 	}
 
 	// apply move-to-front decoding, reading from standard input and writing to
@@ -25,9 +37,10 @@ public class MoveToFront {
 	// if args[0] is '-', apply move-to-front encoding
 	// if args[0] is '+', apply move-to-front decoding
 	public static void main(String[] args) {
-		if (args.length != 1) {
-			throw new IllegalArgumentException("Please enter '-' or '+'");
-		}
+		/*
+		 * if (args.length != 1) { throw new
+		 * IllegalArgumentException("Please enter '-' or '+'"); }
+		 */
 		if (args[0].equals(String.valueOf('-'))) {
 			encode();
 		} else if (args[0].equals(String.valueOf('+'))) {
