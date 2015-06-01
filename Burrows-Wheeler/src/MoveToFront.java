90b7b4a816d92638f0068548fe542ef1f1d2f8a9
Yongzheng Huang <iamhuangyz@gmail.com>
2015-05-31 20:13:19 -0400

    debug

--- Burrows-Wheeler/src/MoveToFront.java
+++ Burrows-Wheeler/src/MoveToFront.java
@@ -1,46 +1,66 @@
-import java.util.HashMap;
-
 public class MoveToFront {
 	// apply move-to-front encoding, reading from standard input and writing to
 	// standard output
 	public static void encode() {
-		HashMap<Character, Integer> sequence = new HashMap<Character, Integer>(
-				256);
-		char first = (char) 0;
-		for (int i = 0; i < 256; i++) {
-			sequence.put((char) i, i);
-		}		
+		// initialize the original sequence
+		char[] sequence = initSequence();
+
 		while (!BinaryStdIn.isEmpty()) {
 			char c = BinaryStdIn.readChar(8);
-			int index = sequence.get(c);
-			// BinaryStdOut.write(index);			
-			System.out.println(index);
-			exch(first, c, sequence);
-			first = c;
+			int index = 0;
+
+			for (int i = 0; i < sequence.length; i++) {
+				if (sequence[i] == c) {
+					index = i;
+					break;
+				}
+			}
+			
+			BinaryStdOut.write(index, 8);
+			move(index, sequence);
 		}
-		// BinaryStdOut.flush();
+		BinaryStdOut.close();
 	}
 
-	private static void exch(char first, char c,
-			HashMap<Character, Integer> sequence) {
-		int temp = sequence.get(first);
-		sequence.put(first, sequence.get(c));
-		sequence.put(c, temp);
+	private static char[] initSequence() {
+		int R = 256; // number of ASCII characters
+		char[] sequence = new char[R];
+		for (int i = 0; i < R; i++) {
+			sequence[i] = (char) i;
+		}
+		return sequence;
+	}
+
+	private static void move(int index, char[] sequence) {
+		char c = sequence[index];
+		// move all entry before c one index further
+		for (int i = index; i > 0; i--) {
+			sequence[i] = sequence[i - 1];
+		}
+		// put c in the first entry
+		sequence[0] = c;
 	}
 
 	// apply move-to-front decoding, reading from standard input and writing to
 	// standard output
 	public static void decode() {
+		char[] sequence = initSequence();
 
+		while (!BinaryStdIn.isEmpty()) {
+			int index = BinaryStdIn.readInt(8);
+			BinaryStdOut.write(sequence[index], 8);
+			move(index, sequence);
+		}
+		BinaryStdOut.close();
 	}
 
 	// if args[0] is '-', apply move-to-front encoding
 	// if args[0] is '+', apply move-to-front decoding
 	public static void main(String[] args) {
-		/*
-		 * if (args.length != 1) { throw new
-		 * IllegalArgumentException("Please enter '-' or '+'"); }
-		 */
+		if (args.length != 1) {
+			throw new IllegalArgumentException("Please enter '-' or '+'");
+		}
+
 		if (args[0].equals(String.valueOf('-'))) {
 			encode();
 		} else if (args[0].equals(String.valueOf('+'))) {
